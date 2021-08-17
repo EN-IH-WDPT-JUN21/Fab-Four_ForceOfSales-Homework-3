1122c4f8b0c90c1b69088b0ebcfaacf9e581105f
Anna NodzyÅnska <nodzynska@gmail.com>
2021-08-17 11:45:15 +0200

    Add Spring project

---
+++ src/main/java/com/ironhack/FabFour_ForceOfSalesHomework3/FabFourForceOfSalesHomework3Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.FabFour_ForceOfSalesHomework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabFourForceOfSalesHomework3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabFourForceOfSalesHomework3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/FabFour_ForceOfSalesHomework3/FabFourForceOfSalesHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.FabFour_ForceOfSalesHomework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabFourForceOfSalesHomework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
