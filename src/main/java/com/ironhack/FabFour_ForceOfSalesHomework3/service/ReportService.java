31ec690dce7a9813a9ec13cb1432c2adc0b8b6ed
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-09-08 23:17:50 +0100

    Reports by SalesRep added to handleCommand() method

--- src/main/java/com/ironhack/FabFour_ForceOfSalesHomework3/service/ReportService.java
+++ src/main/java/com/ironhack/FabFour_ForceOfSalesHomework3/service/ReportService.java
@@ -3,15 +3,23 @@ package com.ironhack.FabFour_ForceOfSalesHomework3.service;
 import com.ironhack.FabFour_ForceOfSalesHomework3.dao.LeadObject;
 import com.ironhack.FabFour_ForceOfSalesHomework3.dao.Opportunity;
 import com.ironhack.FabFour_ForceOfSalesHomework3.dao.SalesRep;
-import com.ironhack.FabFour_ForceOfSalesHomework3.enums.ReportType;
+import com.ironhack.FabFour_ForceOfSalesHomework3.enums.Command;
 import com.ironhack.FabFour_ForceOfSalesHomework3.enums.Status;
+import com.ironhack.FabFour_ForceOfSalesHomework3.enums.TextColor;
 import com.ironhack.FabFour_ForceOfSalesHomework3.repository.LeadObjectRepository;
 import com.ironhack.FabFour_ForceOfSalesHomework3.repository.OpportunityRepository;
 import com.ironhack.FabFour_ForceOfSalesHomework3.repository.SalesRepRepository;
+import com.ironhack.FabFour_ForceOfSalesHomework3.service.impl.EmployeeCountService;
+import com.ironhack.FabFour_ForceOfSalesHomework3.service.impl.OpportunityPerAccountService;
+import com.ironhack.FabFour_ForceOfSalesHomework3.service.impl.ProductQuantityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Scanner;
+
+import static com.ironhack.FabFour_ForceOfSalesHomework3.enums.Command.*;
+import static com.ironhack.FabFour_ForceOfSalesHomework3.service.InputOutputService.colorMessage;
 
 @Service
 public class ReportService {
@@ -20,144 +28,68 @@ public class ReportService {
     private static LeadObjectRepository leadObjectRepository;
     private static OpportunityRepository opportunityRepository;
 
-    public static void reports() {
-        String defaultResponse = "Command not recognised. Try again or type: help";
-        try {
-            System.out.println("Please enter your required report type:");
-            Scanner aScanner = new Scanner(System.in);
-            System.out.println(
-                    "Type one of the below statements to execute:\n\n" +
-                            "REPORT BY SALESREP\n" +
-                            " > Report Lead By SalesRep \n" +
-                            " > Report Opportunity By SalesRep \n" +
-                            " > Report CLOSED_WON by SalesRep \n" +
-                            " > Report CLOSED_LOST by SalesRep \n" +
-                            " > Report OPEN by SalesRep \n" +
-
-                            "\nREPORT BY PRODUCT\n" +
-                            " > Report Opportunity by the product \n" +
-                            " > Report CLOSED_WON by the product \n" +
-                            " > Report CLOSED_LOST by the product \n" +
-                            " > Report OPEN by the product \n" +
-
-                            "\nREPORT BY COUNTRY\n" +
-                            " > Report Opportunity by Country \n" +
-                            " > Report CLOSED_WON by Country \n" +
-                            " > Report CLOSED_LOST by Country \n" +
-                            " > Report OPEN by Country \n" +
-
-                            "\nREPORT BY CITY\n" +
-                            " > Report Opportunity by City \n" +
-                            " > Report CLOSED_WON by City \n" +
-                            " > Report CLOSED_LOST by City \n" +
-                            " > Report OPEN by City \n" +
-
-                            "\nREPORT BY INDUSTRY\n" +
-                            " > Report Opportunity by Industry \n" +
-                            " > Report CLOSED-WON by Industry \n" +
-                            " > Report CLOSED-LOST by Industry \n" +
-                            " > Report OPEN by Industry \n" +
-
-                            "\nREPORT BY EMPLOYEE COUNT\n" +
-                            " > Mean EmployeeCount \n" +
-                            " > Median EmployeeCount \n" +
-                            " > Max EmployeeCount \n" +
-                            " > Min EmployeeCount \n" +
-
-                            "\nREPORT BY QUANTITY\n" +
-                            " > Mean Quantity \n" +
-                            " > Median Quantity \n" +
-                            " > Max Quantity \n" +
-                            " > Min Quantity \n" +
-
-                            "\nSHOW ALL DATA PER TYPE\n" +
-                            " > Mean Opps per Account \n" +
-                            " > Median Opps per Account \n" +
-                            " > Max Opps per Account \n" +
-                            " > Min Opps per Account \n" +
-
-                            "\nTO QUIT\n" +
-                            " > quit - to leave this menu"
-            );
-            ReportType userInput = ReportType.getReportType(aScanner.nextLine().toUpperCase());
-            switch (userInput) {
-                case LEADBYSALESREP:
-                case OPPBYSALESREP:
-                case WONBYSALESREP:
-                case LOSTBYSALESREP:
-                case OPENBYSALESREP:
-                    reportBySalesRep(userInput);
-            }
-        }
-        catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
+    @Autowired
+    public ReportService(SalesRepRepository salesRepRepository, LeadObjectRepository leadObjectRepository, OpportunityRepository opportunityRepository) {
+        this.salesRepRepository = salesRepRepository;
+        this.leadObjectRepository = leadObjectRepository;
+        this.opportunityRepository = opportunityRepository;
     }
 
-
-    public static void reportBySalesRep(ReportType userInput) {
-        List<SalesRep> salesList = salesRepRepository.findAll();
-        try {
-            switch (userInput) {
-                case LEADBYSALESREP:
-                    reportLeadsBySalesRep(salesList);
-                    break;
-                case OPPBYSALESREP:
-                    reportOpportunitiesBySalesRep(salesList);
-                    break;
-                case WONBYSALESREP:
-                    reportOpportunitiesBySalesRepAndStatus(salesList,Status.CLOSED_WON);
-                    break;
-                case LOSTBYSALESREP:
-                    reportOpportunitiesBySalesRepAndStatus(salesList,Status.CLOSED_LOST);
-                    break;
-                case OPENBYSALESREP:
-                    reportOpportunitiesBySalesRepAndStatus(salesList,Status.OPEN);
-                    break;
-            }
-        }
-        catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
-    }
-
-    public static void reportLeadsBySalesRep(List<SalesRep> salesReps) {
+    public static void reportLeadsBySalesRep() {
+        List<SalesRep> salesReps = salesRepRepository.findAll();
         List<LeadObject> leadObjectList = leadObjectRepository.findAll();
-        String printFormat = "| %-10d | %-25s |%n";
-        System.out.println("Here are the number of leads per SalesRep:" + "\n");
-        System.out.format("+------------+---------------------------+%n");
-        System.out.format("| Name       | Number of Leads           |%n");
-        System.out.format("+------------+---------------------------+%n");
-        for (SalesRep sales : salesReps) {
-            System.out.format(printFormat, sales.getName(), leadObjectRepository.countLeadObjectsBySalesRep(sales.getName()));
+        if (salesReps.size() < 1) {
+            colorMessage("+--- There are no SalesReps in the system ---+", TextColor.RED);
         }
-        System.out.format("+------------+---------------------------+%n");
+        else {
+            String printFormat = "| %-10s | %-25d |%n";
+            System.out.println("Here are the number of leads per SalesRep:" + "\n");
+            System.out.format("+------------+---------------------------+%n");
+            System.out.format("| Name       | Number of Leads           |%n");
+            System.out.format("+------------+---------------------------+%n");
+            for (SalesRep sales : salesReps) {
+                System.out.format(printFormat, sales.getName().toUpperCase(), leadObjectRepository.countLeadObjectsBySalesRep(sales.getName()));
+            }
+            System.out.format("+------------+---------------------------+%n");
+        }
     }
 
-    public static void reportOpportunitiesBySalesRep(List<SalesRep> salesReps) {
+    public static void reportOpportunitiesBySalesRep() {
+        List<SalesRep> salesReps = salesRepRepository.findAll();
         List<Opportunity> opportunityList = opportunityRepository.findAll();
-        String printFormat = "| %-10d | %-25s |%n";
-        System.out.println("Here are the number of opportunities per SalesRep:" + "\n");
-        System.out.format("+------------+---------------------------+%n");
-        System.out.format("| Name       | Number of Opportunities   |%n");
-        System.out.format("+------------+---------------------------+%n");
-        for (SalesRep sales : salesReps) {
-            System.out.format(printFormat, sales.getName(), opportunityRepository.countOpportunitiesBySalesRep(sales.getName()));
+        if (salesReps.size() < 1) {
+            colorMessage("+--- There are no SalesReps in the system ---+", TextColor.RED);
         }
-        System.out.format("+------------+---------------------------+%n");
+        else {
+            String printFormat = "| %-10s | %-25d |%n";
+            System.out.println("Here are the number of opportunities per SalesRep:" + "\n");
+            System.out.format("+------------+---------------------------+%n");
+            System.out.format("| Name       | Number of Opportunities   |%n");
+            System.out.format("+------------+---------------------------+%n");
+            for (SalesRep sales : salesReps) {
+                System.out.format(printFormat, sales.getName().toUpperCase(), opportunityRepository.countOpportunitiesBySalesRep(sales.getName()));
+            }
+            System.out.format("+------------+---------------------------+%n");
+        }
     }
 
-    public static void reportOpportunitiesBySalesRepAndStatus(List<SalesRep> salesReps, Status status) {
+    public static void reportOpportunitiesBySalesRepAndStatus(Status status) {
+        List<SalesRep> salesReps = salesRepRepository.findAll();
         List<Opportunity> opportunityList = opportunityRepository.findAll();
-        String convStatus = status.toString();
-        String printFormat = "| %-10d | %-25s |%n";
-        System.out.println(String.format("Here are the number of %b opportunities per SalesRep: \n", convStatus));
-        System.out.format("+------------+---------------------------+%n");
-        System.out.format("| Name       | Number of Opportunities   |%n");
-        System.out.format("+------------+---------------------------+%n");
-        for (SalesRep sales : salesReps) {
-            System.out.format(printFormat, sales.getName(),opportunityRepository.countOpportunitiesBySalesRepAndStatus(sales.getName(),convStatus));
+        if (salesReps.size() < 1) {
+            colorMessage("+--- There are no SalesReps in the system ---+", TextColor.RED);
         }
-        System.out.format("+------------+---------------------------+%n");
+        else {
+            String convStatus = status.name();
+            String printFormat = "| %-10s | %-25d |%n";
+            System.out.println(String.format("Here are the number of %b opportunities per SalesRep: \n", convStatus));
+            System.out.format("+------------+---------------------------+%n");
+            System.out.format("| Name       | Number of Opportunities   |%n");
+            System.out.format("+------------+---------------------------+%n");
+            for (SalesRep sales : salesReps) {
+                System.out.format(printFormat, sales.getName().toUpperCase(), opportunityRepository.countOpportunitiesBySalesRepAndStatus(sales.getName(), convStatus));
+            }
+            System.out.format("+------------+---------------------------+%n");
+        }
     }
 }
